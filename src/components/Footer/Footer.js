e5bd1f4dcf17c749090e4783c5faaec4304e6e5e
Rimma27 <rimmaady@yandex.ru>
2023-03-02 13:40:45 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,15 @@ import line from '../../images/line.svg';
 
 function Footer() {
     return (
-        <footer className="footer">
+        <footer>
             <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-            <img src={line} alt="Линия" />
-            <h3 className="footer__copyright">© 2020</h3>
-            <div className="footer__links">
-                <Link to="">Яндекс.Практикум</Link>
-                <Link to="https://github.com/Rimma27">Github</Link>
+            <img src={line} className="footer__line" alt="Линия" />
+            <div className="footer__basement">
+                <h3 className="footer__copyright">© 2020</h3>
+                <div className="footer__links">
+                    <Link className="footer__yandex" to="">Яндекс.Практикум</Link>
+                    <Link className="footer__github" to="https://github.com/Rimma27">Github</Link>
+                </div>
             </div>
         </footer>
     )
