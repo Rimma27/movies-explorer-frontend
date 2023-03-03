01fb609fc76c3713634f68f2325496a7d7880174
Rimma27 <rimmaady@yandex.ru>
2023-03-03 15:45:20 +0300

    searchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import SearchForm from "./SearchForm";
 
 function Movies () {
     return(
         <>
         <Header />
+        <SearchForm />
         </>
     )
 }
--- src/components/Movies/SearchForm.js
+++ src/components/Movies/SearchForm.js
@@ -1,15 +1,20 @@
 import React from "react";
 import search from '../../images/find.jpg';
+import filter from '../../images/filterON.jpg';
 
 function SearchForm() {
     return (
-        <>
-            <form className="search" placeholder="Фильм">
+        <div className="search">
+            <form className="search__form" placeholder="Фильм">
                 <button type="submit" className="search__button">
                     <img src={search} alt="Поиск" />
                 </button>
             </form>
-        </>
+            <h2 className="search__shortFilms">Короткометражки</h2>
+            <button className="search__filter">
+                <img src={filter} alt="Фильтр" />
+                </button> 
+        </div>
     )
 }
 
