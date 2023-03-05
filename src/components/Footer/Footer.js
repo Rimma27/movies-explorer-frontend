30f4eadbc01ef366aebbdd32964de215469864cb
Rimma27 <rimmaady@yandex.ru>
2023-03-05 12:24:01 +0300

    finish landing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</div>
-            <div className="footer__basement">
+            <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+           
                 <div className="footer__links">
                     <Link className="footer__link" to="">Яндекс.Практикум</Link>
                     <Link className="footer__link" to="https://github.com/Rimma27">Github</Link>
                 </div>
                 <h3 className="footer__copyright">© 2020</h3>
-            </div>
+          
         </footer>
     )
 };
