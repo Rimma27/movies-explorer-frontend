aa5b062f6189ae787f0e3dfe5dd4bb97e51bbdf0
Rimma27 <rimmaady@yandex.ru>
2023-04-17 14:36:55 +0300

    404 button goback

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function PageNotFound() {
+    const navigate = useNavigate(); 
+
+    function goBack() {
+        navigate(-1);
+    }
     return (
         <>
             <h2 className="not-found__title">404</h2>
             <h3 className="not-found__subtitle">Страница не найдена</h3>
-            <Link className="button" to="/">Назад</Link>
+            <button onClick={goBack} className="button">Назад</button>
         </>
     )
 }
